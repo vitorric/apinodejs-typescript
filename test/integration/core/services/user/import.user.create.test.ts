a543c0dbfe1d0a1d4a389fc1a95dd82fb4d9d80a
Vitor Ricardo <vitorricardo@outlook.com>
2022-06-09 22:45:17 -0300

    feat: test user email already exists

--- test/integration/core/services/user/import.user.create.test.ts
+++ test/integration/core/services/user/import.user.create.test.ts
@@ -7,19 +7,33 @@ import { hooks } from '../../../../hooks';
 hooks();
 
 const userService: UserService = UserFactory.getService();
+const email = 'vitorricardo@email.com';
 
 describe('Create User', () => {
   it('successfully', async () => {
     const user = await userService.create({
-      email: 'vitorricardo@email.com',
+      email,
       role: Roles.User,
     });
 
     expect(user.statusCode).toEqual(200);
     expect(user.success).toBeTruthy();
     expect(user.payload).toMatchObject({
-      email: 'vitorricardo@email.com',
+      email,
       role: Roles.User.toString(),
+    });
+  });
+
+  it('email already exists', async () => {
+    const user = await userService.create({
+      email,
+      role: Roles.User,
+    });
+
+    expect(user.statusCode).toEqual(409);
+    expect(user.success).toBeFalsy();
+    expect(user.payload).toMatchObject({
+      error: 'User email already exists.',
     });
   });
 });
